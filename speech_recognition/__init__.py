cf902fca337ac69ed601f0527c1a099b8ac8836f
Uberi <azhang9@gmail.com>
2015-04-04 01:00:15 -0400

    Bump version.

--- speech_recognition/__init__.py
+++ speech_recognition/__init__.py
@@ -3,7 +3,7 @@
 """Library for performing speech recognition with the Google Speech Recognition API."""
 
 __author__ = "Anthony Zhang (Uberi)"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __license__ = "BSD"
 
 import io, os, subprocess, wave
