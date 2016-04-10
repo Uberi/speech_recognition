a57ac652f9c06a0931f45ccbe7430515721ff448
Anthony Zhang <azhang9@gmail.com>
2016-04-09 20:16:12 -0400

    Update build script

--- build.bat
+++
@@ -1,4 +0,0 @@
-python setup.py sdist
-
-echo "if the following doesn't work, make sure you have your account set up properly with `python setup.py register`"
-python setup.py sdist upload
--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-python3 setup.py sdist
-
 echo "if the following doesn't work, make sure you have your account set up properly with `python3 setup.py register`"
-python3 setup.py sdist upload
+python3 setup.py sdist upload --sign
