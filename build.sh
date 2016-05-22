45f003238b36520403973c346e6055dd8f41ff78
Anthony Zhang <azhang9@gmail.com>
2016-05-22 16:27:15 -0400

    Work around some setuptools limitations with using GPG versions

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
 echo "if the following doesn't work, make sure you have your account set up properly with `python3 setup.py register`"
-python3 setup.py sdist upload --sign
+
+# make sure we use GnuPG 2 rather than GnuPG 1
+sudo ln "$(which gpg2)" dist/gpg
+PATH=./dist:$PATH python3 setup.py sdist upload --sign
