241d6a9fb689f3be3be8c240be50b4b204ac1194
Tim Eccleston <teccleston@axialhealthcare.com>
2016-06-22 14:18:39 -0500

    applied examples/audio_transcribe to Interviews (redacted)

--- examples/audio_transcribe.py
+++ examples/audio_transcribe.py
@@ -2,9 +2,25 @@
 
 import speech_recognition as sr
 
+import sys
+args = sys.argv[1:]
+
+if not args:
+    print 'usage: <file.aif> (to transcribe)'
+    sys.exit(1)
+
+path = args[0]
+
+main_directory = 'Interviews/'
+split = path.split(main_directory)
+main_path = split[0] + main_directory
+specific_path = split[1]
+
+write_path = specific_path.replace(r'.band/Media/', '__').replace(r'.aif', '').replace(r' ', '_')
+
 # obtain path to "english.wav" in the same folder as this script
 from os import path
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "english.wav")
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), path.join(main_path,specific_path))
 #AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
 #AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "chinese.flac")
 
@@ -15,56 +31,61 @@ with sr.AudioFile(AUDIO_FILE) as source:
 
 # recognize speech using Sphinx
 try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    text = r.recognize_sphinx(audio)
+    # print("Sphinx thinks you said " + r.recognize_sphinx(audio))
 except sr.UnknownValueError:
-    print("Sphinx could not understand audio")
+    text = "Sphinx could not understand audio"
 except sr.RequestError as e:
-    print("Sphinx error; {0}".format(e))
+    text = "Sphinx error; {0}".format(e)
+except Error as e:
+    text = e
+with open(main_path[3:] + write_path + '__sphinx.txt', 'wb') as f:
+    f.write(text)
 
-# recognize speech using Google Speech Recognition
-try:
-    # for testing purposes, we're just using the default API key
-    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-    # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+# # recognize speech using Google Speech Recognition
+# try:
+#     # for testing purposes, we're just using the default API key
+#     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+#     # instead of `r.recognize_google(audio)`
+#     print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+# except sr.UnknownValueError:
+#     print("Google Speech Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-# recognize speech using Wit.ai
-WIT_AI_KEY = "INSERT WIT.AI API KEY HERE" # Wit.ai keys are 32-character uppercase alphanumeric strings
-try:
-    print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
-except sr.UnknownValueError:
-    print("Wit.ai could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Wit.ai service; {0}".format(e))
-
-# recognize speech using Microsoft Bing Voice Recognition
-BING_KEY = "INSERT BING API KEY HERE" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
-try:
-    print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
-except sr.UnknownValueError:
-    print("Microsoft Bing Voice Recognition could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
-
-# recognize speech using api.ai
-API_AI_CLIENT_ACCESS_TOKEN = "INSERT API.AI API KEY HERE" # api.ai keys are 32-character lowercase hexadecimal strings
-try:
-    print("api.ai thinks you said " + r.recognize_api(audio, client_access_token=API_AI_CLIENT_ACCESS_TOKEN))
-except sr.UnknownValueError:
-    print("api.ai could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from api.ai service; {0}".format(e))
-
-# recognize speech using IBM Speech to Text
-IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE" # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
-IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE" # IBM Speech to Text passwords are mixed-case alphanumeric strings
-try:
-    print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
-except sr.UnknownValueError:
-    print("IBM Speech to Text could not understand audio")
-except sr.RequestError as e:
-    print("Could not request results from IBM Speech to Text service; {0}".format(e))
+# # recognize speech using Wit.ai
+# WIT_AI_KEY = "INSERT WIT.AI API KEY HERE" # Wit.ai keys are 32-character uppercase alphanumeric strings
+# try:
+#     print("Wit.ai thinks you said " + r.recognize_wit(audio, key=WIT_AI_KEY))
+# except sr.UnknownValueError:
+#     print("Wit.ai could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Wit.ai service; {0}".format(e))
+#
+# # recognize speech using Microsoft Bing Voice Recognition
+# BING_KEY = "INSERT BING API KEY HERE" # Microsoft Bing Voice Recognition API keys 32-character lowercase hexadecimal strings
+# try:
+#     print("Microsoft Bing Voice Recognition thinks you said " + r.recognize_bing(audio, key=BING_KEY))
+# except sr.UnknownValueError:
+#     print("Microsoft Bing Voice Recognition could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from Microsoft Bing Voice Recognition service; {0}".format(e))
+#
+# # recognize speech using api.ai
+# API_AI_CLIENT_ACCESS_TOKEN = "INSERT API.AI API KEY HERE" # api.ai keys are 32-character lowercase hexadecimal strings
+# try:
+#     print("api.ai thinks you said " + r.recognize_api(audio, client_access_token=API_AI_CLIENT_ACCESS_TOKEN))
+# except sr.UnknownValueError:
+#     print("api.ai could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from api.ai service; {0}".format(e))
+#
+# # recognize speech using IBM Speech to Text
+# IBM_USERNAME = "INSERT IBM SPEECH TO TEXT USERNAME HERE" # IBM Speech to Text usernames are strings of the form XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+# IBM_PASSWORD = "INSERT IBM SPEECH TO TEXT PASSWORD HERE" # IBM Speech to Text passwords are mixed-case alphanumeric strings
+# try:
+#     print("IBM Speech to Text thinks you said " + r.recognize_ibm(audio, username=IBM_USERNAME, password=IBM_PASSWORD))
+# except sr.UnknownValueError:
+#     print("IBM Speech to Text could not understand audio")
+# except sr.RequestError as e:
+#     print("Could not request results from IBM Speech to Text service; {0}".format(e))
