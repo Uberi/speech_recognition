e452e9f3295232a6f5de0dc789acf2e1a4311f5c
Norbert Preining <norbert@preining.info>
2020-01-05 00:01:58 +0900

    bump version number for gemfury upload

--- speech_recognition/__init__.py
+++ speech_recognition/__init__.py
@@ -22,7 +22,7 @@ import time
 import uuid
 
 __author__ = "Anthony Zhang (Uberi)"
-__version__ = "3.8.1"
+__version__ = "3.8.1.fossasia-1"
 __license__ = "BSD"
 
 try:  # attempt to use the Python 2 modules
