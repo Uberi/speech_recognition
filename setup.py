6474bf7a1346b648dc4b20913787132c3194167b
ftnext <takuyafjp+develop@gmail.com>
2022-12-04 21:41:42 +0900

    [bugfix] twine upload failed: The name isn't allowed

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ class InstallWithExtraSteps(install):
 
 
 setup(
-    name="speech-recognition",
+    name="SpeechRecognition",
     version=speech_recognition.__version__,
     packages=["speech_recognition"],
     include_package_data=True,
