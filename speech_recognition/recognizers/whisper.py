af58e70ebe53715e36f86e82fbc7b599abe5d8c4
ftnext <takuyafjp+develop@gmail.com>
2023-03-02 22:22:32 +0900

    [docs] Make more developer-friendly

--- speech_recognition/recognizers/whisper.py
+++ speech_recognition/recognizers/whisper.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import os
 from io import BytesIO
 
@@ -5,8 +7,19 @@ from speech_recognition.audio import AudioData
 
 
 def recognize_whisper_api(
-    recognizer, audio_data, *, model="whisper-1", api_key=None
+    recognizer,
+    audio_data: "AudioData",
+    *,
+    model: str = "whisper-1",
+    api_key: str | None = None,
 ):
+    """
+    Performs speech recognition on ``audio_data`` (an ``AudioData`` instance), using the OpenAI Whisper API.
+
+    This function requires an OpenAI account; visit https://platform.openai.com/signup, then generate API Key in `User settings <https://platform.openai.com/account/api-keys>`__.
+
+    Detail: https://platform.openai.com/docs/guides/speech-to-text
+    """
     if not isinstance(audio_data, AudioData):
         raise ValueError("``audio_data`` must be an ``AudioData`` instance")
     if api_key is None and os.environ.get("OPENAI_API_KEY") is None:
