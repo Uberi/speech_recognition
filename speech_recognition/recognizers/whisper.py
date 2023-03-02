7116a5d4c0fdf9ecca0c7a9503f0bab3c099ffe8
ftnext <takuyafjp+develop@gmail.com>
2023-03-02 22:20:56 +0900

    [feat] Raise error in wrong usage

--- speech_recognition/recognizers/whisper.py
+++ speech_recognition/recognizers/whisper.py
@@ -1,9 +1,17 @@
+import os
 from io import BytesIO
+
+from speech_recognition.audio import AudioData
 
 
 def recognize_whisper_api(
     recognizer, audio_data, *, model="whisper-1", api_key=None
 ):
+    if not isinstance(audio_data, AudioData):
+        raise ValueError("``audio_data`` must be an ``AudioData`` instance")
+    if api_key is None and os.environ.get("OPENAI_API_KEY") is None:
+        raise ValueError("Set environment variable ``OPENAI_API_KEY``")
+
     import openai
 
     wav_data = BytesIO(audio_data.get_wav_data())
