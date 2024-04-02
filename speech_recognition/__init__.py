a0457e65790d168d2be754ee190605226c4300d3
nikkie <takuyafjp+develop@gmail.com>
2024-04-02 10:05:23 +0000

    [style] Same indent (E128)

--- speech_recognition/__init__.py
+++ speech_recognition/__init__.py
@@ -1087,7 +1087,8 @@ class Recognizer(AudioSource):
             aws_secret_access_key=secret_access_key,
             region_name=region)
 
-        s3 = boto3.client('s3',
+        s3 = boto3.client(
+            's3',
             aws_access_key_id=access_key_id,
             aws_secret_access_key=secret_access_key,
             region_name=region)
