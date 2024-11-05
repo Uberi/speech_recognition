5cc9ceedbcf6713cd82a2117d650c93bf715390e
nikkie <takuyafjp+develop@gmail.com>
2024-11-05 23:58:16 +0000

    bugfix: Skip PyAudio tests on WIndows

--- tests/test_microphone.py
+++ tests/test_microphone.py
@@ -1,8 +1,12 @@
+import sys
 from unittest.mock import patch
+
+import pytest
 
 from speech_recognition.microphone import PyAudioWrapper
 
 
+@pytest.mark.skip(sys.platform.startswith("win"), "skip on Windows")
 class TestPyAudioWrapper:
     @patch("pyaudio.PyAudio")
     def test_get_pyaudio(self, PyAudio):
