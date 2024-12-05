21acf32a02ea3db68e4b3b23a6fafe6d2a088bfd
ftnext <takuyafjp+develop@gmail.com>
2024-12-06 08:45:49 +0900

    [test] Implementation with RESPX

--- tests/recognizers/test_openai.py
+++ tests/recognizers/test_openai.py
@@ -1,8 +1,32 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, patch
 
+import httpx
+import respx
+
 from speech_recognition import AudioData, Recognizer
 from speech_recognition.recognizers import whisper
+
+
+@respx.mock(assert_all_called=True, assert_all_mocked=True)
+def test_transcribe_with_openai_whisper(respx_mock, monkeypatch):
+    monkeypatch.setenv("OPENAI_API_KEY", "sk_openai_api_key")
+
+    respx_mock.post("https://api.openai.com/v1/audio/transcriptions").mock(
+        return_value=httpx.Response(
+            200,
+            json={"text": "Transcription by OpenAI Whisper"},
+        )
+    )
+
+    audio_data = MagicMock(spec=AudioData)
+    audio_data.get_wav_data.return_value = b"audio_data"
+
+    actual = whisper.recognize_whisper_api(
+        MagicMock(spec=Recognizer), audio_data, model="whisper-1"
+    )
+
+    assert actual == "Transcription by OpenAI Whisper"
 
 
 @patch("speech_recognition.recognizers.whisper.os.environ")
