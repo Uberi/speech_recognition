b792f18ac57d1e2dc76550e14c78310b1fccd910
ftnext <takuyafjp+develop@gmail.com>
2024-12-21 15:49:52 +0900

    [refactor] Tweak sut

--- tests/recognizers/test_google_cloud.py
+++ tests/recognizers/test_google_cloud.py
@@ -10,6 +10,7 @@ from google.cloud.speech import (
 
 from speech_recognition import Recognizer
 from speech_recognition.audio import AudioData
+from speech_recognition.recognizers.google_cloud import recognize
 
 
 @patch("google.cloud.speech.SpeechClient")
@@ -37,7 +38,7 @@ def test_transcribe_with_google_cloud_speech(SpeechClient, monkeypatch):
     audio_data.sample_rate = 16_000
     audio_data.get_flac_data.return_value = b"flac_data"
 
-    actual = Recognizer().recognize_google_cloud(audio_data)
+    actual = recognize(MagicMock(spec=Recognizer), audio_data)
 
     assert actual == "how old is the Brooklyn Bridge "
     SpeechClient.assert_called_once_with()
