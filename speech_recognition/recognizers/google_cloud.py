43a440aa1cb3a4b0bbc684d9007b278462f6c2d2
ftnext <takuyafjp+develop@gmail.com>
2024-12-25 19:04:28 +0900

    [docs] Refine docs
    * Update links

--- speech_recognition/recognizers/google_cloud.py
+++ speech_recognition/recognizers/google_cloud.py
@@ -7,17 +7,38 @@ from speech_recognition.audio import AudioData
 from speech_recognition.exceptions import RequestError, UnknownValueError
 
 if TYPE_CHECKING:
-    from google.cloud.speech_v1.types import RecognitionConfig, SpeechContext
+    from google.cloud.speech_v1.types import (
+        RecognitionConfig,
+        RecognizeResponse,
+        SpeechContext,
+    )
     from typing_extensions import Required
 
 
 class GoogleCloudRecognizerParameters(TypedDict, total=False):
+    """Optional parameters.
+
+    The recognition language is determined by ``language_code``, which is a BCP-47 language tag like ``"en-US"`` (US English). Default: ``"en-US"``.
+    A list of supported language tags can be found in the `Speech-to-Text supported languages <https://cloud.google.com/speech/docs/languages>`__.
+
+    If ``preferred_phrases`` is an iterable of phrase strings, those given phrases will be more likely to be recognized over similar-sounding alternatives.
+    This is useful for things like keyword/command recognition or adding new phrases that aren't in Google's vocabulary.
+    Note that the API imposes certain `restrictions on the list of phrase strings <https://cloud.google.com/speech/limits#content>`__.
+
+    ``show_all``: See :py:func:`recognize`.
+
+    ``model``: You can select the model to get best results. (See `RecognitionConfig's documentation <https://cloud.google.com/python/docs/reference/speech/latest/google.cloud.speech_v1.types.RecognitionConfig>`__ for detail)
+
+    ``use_enhanced``: Set to true to use an enhanced model for speech recognition.
+    """
+
     # SpeechRecognition specific parameters
     preferred_phrases: list[str]
     show_all: bool
 
     # Speech-to-Text V1 API's parameters
     language_code: str
+    model: str
     use_enhanced: bool
     # TODO Add others support
 
@@ -33,6 +54,7 @@ class GoogleCloudSpeechV1Parameters(TypedDict, total=False):
     language_code: Required[str]
     speech_contexts: list[SpeechContext]
     enable_word_time_offsets: bool
+    model: str
     use_enhanced: bool
 
 
@@ -61,27 +83,14 @@ def recognize(
     audio_data: AudioData,
     credentials_json_path: str | None = None,
     **kwargs: GoogleCloudRecognizerParameters,
-):
+) -> str | RecognizeResponse:
     """Performs speech recognition on ``audio_data`` (an ``AudioData`` instance), using the Google Cloud Speech-to-Text V1 API.
 
-    This function requires a Google Cloud Platform account; see the `Google Cloud Speech API Quickstart <https://cloud.google.com/speech/docs/getting-started>`__ for details and instructions. Basically, create a project, enable billing for the project, enable the Google Cloud Speech API for the project, and set up Service Account Key credentials for the project. The result is a JSON file containing the API credentials. The text content of this JSON file is specified by ``credentials_json``. If not specified, the library will try to automatically `find the default API credentials JSON file <https://developers.google.com/identity/protocols/application-default-credentials>`__.
-
-    The recognition language is determined by ``language_code``, which is a BCP-47 language tag like ``"en-US"`` (US English). A list of supported language tags can be found in the `Google Cloud Speech API documentation <https://cloud.google.com/speech/docs/languages>`__.
-
-    If ``preferred_phrases`` is an iterable of phrase strings, those given phrases will be more likely to be recognized over similar-sounding alternatives. This is useful for things like keyword/command recognition or adding new phrases that aren't in Google's vocabulary. Note that the API imposes certain `restrictions on the list of phrase strings <https://cloud.google.com/speech/limits#content>`__.
-
-    ``api_params`` are Cloud Speech API-specific parameters as dict (optional). For more information see <https://cloud.google.com/python/docs/reference/speech/latest/google.cloud.speech_v1.types.RecognitionConfig>
-
-        The ``use_enhanced`` is a boolean option. If use_enhanced is set to true and the model field is not set,
-        then an appropriate enhanced model is chosen if an enhanced model exists for the audio.
-        If use_enhanced is true and an enhanced version of the specified model does not exist,
-        then the speech is recognized using the standard version of the specified model.
-
-        Furthermore, if the option ``use_enhanced`` has not been set the option ``model`` can be used, which can be used to select the model best
-        suited to your domain to get best results. If a model is not explicitly specified,
-        then we auto-select a model based on the other parameters of this method.
+    This function requires a Google Cloud Platform account; see the `Set up Speech-to-Text <https://cloud.google.com/speech-to-text/docs/before-you-begin>`__ for details and instructions. Basically, create a project, enable billing for the project, enable the Google Cloud Speech API for the project.
+    And create local authentication credentials for your user account. The result is a JSON file containing the API credentials. You can specify the JSON file by ``credentials_json_path``. If not specified, the library will try to automatically `find the default API credentials JSON file <https://developers.google.com/identity/protocols/application-default-credentials>`__.
 
     Returns the most likely transcription if ``show_all`` is False (the default). Otherwise, returns the raw API response as a JSON dictionary.
+    For other parameters, see :py:class:`GoogleCloudRecognizerParameters`.
 
     Raises a ``speech_recognition.UnknownValueError`` exception if the speech is unintelligible. Raises a ``speech_recognition.RequestError`` exception if the speech recognition operation failed, if the credentials aren't valid, or if there is no Internet connection.
     """
