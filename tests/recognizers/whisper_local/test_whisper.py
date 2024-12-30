37980335c1dd0712f2f8e3a17bbeed6ec39337e8
ftnext <takuyafjp+develop@gmail.com>
2024-12-30 10:22:31 +0900

    [test] Use Whisper model response

--- tests/recognizers/whisper_local/test_whisper.py
+++ tests/recognizers/whisper_local/test_whisper.py
@@ -18,7 +18,11 @@ class RecognizeWhisperTestCase(TestCase):
         import numpy as np
 
         whisper_model = load_model.return_value
-        transcript = whisper_model.transcribe.return_value
+        whisper_model.transcribe.return_value = {
+            "text": "Transcription by Whisper model",
+            "language": "en",
+            # Omit "segments"
+        }
         audio_array = MagicMock()
         dummy_sampling_rate = 99_999
         sf_read.return_value = (audio_array, dummy_sampling_rate)
@@ -26,7 +30,7 @@ class RecognizeWhisperTestCase(TestCase):
         audio_data = MagicMock(spec=AudioData)
         actual = recognize(MagicMock(spec=Recognizer), audio_data)
 
-        self.assertEqual(actual, transcript.__getitem__.return_value)
+        assert actual == "Transcription by Whisper model"
         load_model.assert_called_once_with("base")
         audio_data.get_wav_data.assert_called_once_with(convert_rate=16000)
         BytesIO.assert_called_once_with(audio_data.get_wav_data.return_value)
@@ -36,10 +40,27 @@ class RecognizeWhisperTestCase(TestCase):
             audio_array.astype.return_value,
             fp16=is_available.return_value,
         )
-        transcript.__getitem__.assert_called_once_with("text")
 
     def test_return_as_dict(self, load_model, is_available, sf_read, BytesIO):
         whisper_model = load_model.return_value
+        whisper_model.transcribe.return_value = {
+            "text": " 1, 2, 3",
+            "segments": [
+                {
+                    "id": 0,
+                    "seek": 0,
+                    "start": 0.0,
+                    "end": 2.64,
+                    "text": " 1, 2, 3",
+                    "tokens": [50364, 502, 11, 568, 11, 805, 50496],
+                    "temperature": 0.0,
+                    "avg_logprob": -0.5379014015197754,
+                    "compression_ratio": 0.4666666666666667,
+                    "no_speech_prob": 0.17316073179244995,
+                }
+            ],
+            "language": "en",
+        }
         audio_array = MagicMock()
         dummy_sampling_rate = 99_999
         sf_read.return_value = (audio_array, dummy_sampling_rate)
@@ -49,17 +70,35 @@ class RecognizeWhisperTestCase(TestCase):
             MagicMock(spec=Recognizer), audio_data, show_dict=True
         )
 
-        self.assertEqual(actual, whisper_model.transcribe.return_value)
+        expected = {
+            "text": " 1, 2, 3",
+            "segments": [
+                {
+                    "id": 0,
+                    "seek": 0,
+                    "start": 0.0,
+                    "end": 2.64,
+                    "text": " 1, 2, 3",
+                    "tokens": [50364, 502, 11, 568, 11, 805, 50496],
+                    "temperature": 0.0,
+                    "avg_logprob": -0.5379014015197754,
+                    "compression_ratio": 0.4666666666666667,
+                    "no_speech_prob": 0.17316073179244995,
+                }
+            ],
+            "language": "en",
+        }
+
+        assert actual == expected
 
     def test_pass_parameters(self, load_model, is_available, sf_read, BytesIO):
         whisper_model = load_model.return_value
-        transcript = whisper_model.transcribe.return_value
         audio_array = MagicMock()
         dummy_sampling_rate = 99_999
         sf_read.return_value = (audio_array, dummy_sampling_rate)
 
         audio_data = MagicMock(spec=AudioData)
-        actual = recognize(
+        _ = recognize(
             MagicMock(spec=Recognizer),
             audio_data,
             model="small",
@@ -68,7 +107,6 @@ class RecognizeWhisperTestCase(TestCase):
             temperature=0,
         )
 
-        self.assertEqual(actual, transcript.__getitem__.return_value)
         load_model.assert_called_once_with("small")
         whisper_model.transcribe.assert_called_once_with(
             audio_array.astype.return_value,
